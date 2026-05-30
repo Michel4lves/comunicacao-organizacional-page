5988bdf5bbcbe0460a83e96df41075eab36e22ab
Michel4lves <michelsantosa@gmail.com>
2026-05-29 23:37:22 -0300

    tipos de organização concluído

--- comunicacao-organizacional/src/components/cards/CommunicationTypeCard.jsx
+++ comunicacao-organizacional/src/components/cards/CommunicationTypeCard.jsx
@@ -1,18 +1,20 @@
 import "./CommunicationTypeCard.css"
 
-const CommunicationTypeCard = ({ title, subtitle, imageSrc, topics, footer, color }) => {
+const CommunicationTypeCard = ({ title, subtitle, imageSrc, topics, footer_icon, footer_text, color }) => {
     return (
         <div className={`communication-type-card ${color}`}>
             <h3 className="communication-type-card-title">{title}</h3>
             <h5 className="communication-type-card-subtitle">{subtitle}</h5>
             <img className="communication-type-card-image" src={imageSrc} alt={title}/>
-            {/* <p className="communication-type-card-topics">{topics}</p> */}
-            <ul>
+            <ul className="communication-type-card-topics">
                 {topics.map((topic, index) => (
                     <li key={index}>{topic}</li>
                 ))}
             </ul>
-            <p className="communication-type-card-footer">{footer}</p>
+            <div className="communication-type-card-footer">
+                <div className="communication-type-card-footer-icon">{footer_icon}</div>
+                <p className="communication-type-card-footer-text">{footer_text}</p>
+            </div>
         </div>
     )
 }
--- comunicacao-organizacional/src/components/pageParts/CommunicationTypes.jsx
+++ comunicacao-organizacional/src/components/pageParts/CommunicationTypes.jsx
@@ -3,6 +3,14 @@ import "./CommunicationTypes.css"
 
 import comunicacao_descendente from "../../assets/pictures/02_comunicacao_descendente.png"
 import comunicacao_ascendente from "../../assets/pictures/03_comunicacao_ascendente.png"
+import comunicacao_horizontal from "../../assets/pictures/04_comunicacao_horizontal.png"
+import comunicacao_informal from "../../assets/pictures/05_comunicacao_informal.png"
+
+import { FaArrowAltCircleDown } from "react-icons/fa";
+import { FaArrowAltCircleUp } from "react-icons/fa";
+import { HiUsers } from "react-icons/hi2";
+import { PiHandshakeFill } from "react-icons/pi";
+
 
 const CommunicationTypes = () => {
     return (
@@ -18,22 +26,49 @@ const CommunicationTypes = () => {
                             "• Informa mudanças",
                             "• Oferece feedback e treinamentos",
                             "• Promove alinhamento" ]}
-                    footer="Transparência que inspira confiança e engajamento."
-                />
+                    footer_icon={<FaArrowAltCircleDown />}
+                    footer_text="Transparência que inspira confiança e engajamento."
+                    color="blue"
+                    />
                 <CommunicationTypeCard
                     title="COMUNICAÇÃO ASCENDENTE"
                     subtitle="Das equipes para a liderança"
                     imageSrc={comunicacao_ascendente}
                     topics={["• Identifica problemas rapidamente",
-                            "• Traz idéias e sugestões",
-                            "• Melhora processos",
-                            "• Fortalece a confiança",
-                            "• Apoia decisões mais assertivas" ]}
-                    footer="Escuta ativa que gera participação e inovação."
+                        "• Traz idéias e sugestões",
+                        "• Melhora processos",
+                        "• Fortalece a confiança",
+                        "• Apoia decisões mais assertivas" ]}
+                    footer_icon={<FaArrowAltCircleUp />}
+                    footer_text="Escuta ativa que gera participação e inovação."
+                    color="green"
                 />
-                
+                <CommunicationTypeCard
+                    title="COMUNICAÇÃO HORIZONTAL"
+                    subtitle="Entre áreas e profissionais do mesmo nível"
+                    imageSrc={comunicacao_horizontal}
+                    topics={["• Integra departamentos",
+                            "• Coordena atividades",
+                            "• Resolve problemas juntos",
+                            "• Compartilha conhecimentos",
+                            "• Reduz conflitos" ]}
+                    footer_icon={<HiUsers />}
+                    footer_text="Colaboração que fortalece e potencializa resultados."
+                    color="purple"
+                />
+                <CommunicationTypeCard
+                    title="COMUNICAÇÃO INFORMAL"
+                    subtitle="As conexões naturais do ambiente de trabalho"
+                    imageSrc={comunicacao_informal}
+                    topics={["• Conversas espontâneas",
+                            "• Redes de relacionamento",
+                            "• Troca de esperiências",
+                            "• Agilidade na circulação de informações" ]}
+                    footer_icon={<PiHandshakeFill />}
+                    footer_text="Relações fortes que criam um ambiente saudável."
+                    color="orange"
+                />
             </div>
-
         </div>
     )
 }
